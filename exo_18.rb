934613f8badddeb1d22fbebac21e7d2b7c626594
Thomasi13 <40562247+Thomasi13@users.noreply.github.com>
2018-06-28 15:59:42 +0200

    Add files via upload

---
+++ exo_18.rb
@@ -0,0 +1,7 @@
+email_list = []
+
+50.times do |i|
+
+		email_list << "jean.dupont.0#{i+1}@email.fr"
+end	
+ puts email_list
---
+++ exo_19.rb
@@ -0,0 +1,11 @@
+email_list = []
+
+50.times do |i|
+
+		email_list << "jean.dupont.0#{i+1}@email.fr"
+
+		if email_list % 2 == 0
+        	puts email_list
+    	end
+
+end	
