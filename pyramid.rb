ed12168bb6a90be4fbca7581a5d606520bde6ce5
Thomasi13 <40562247+Thomasi13@users.noreply.github.com>
2018-06-28 16:07:19 +0200

    Exo Final

---
+++ pyramid.rb
@@ -0,0 +1,18 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? ( entre 1 et 25)"
+
+print "> "
+user_floor = gets.chomp
+n = 1
+
+while (user_floor.to_i < 1) || (user_floor.to_i > 25)
+	puts " Coquin ! Tu n'as pas rentré une valeur entre 1 et 25, essaye encore"
+	print "> "
+	user_floor = gets.chomp
+end
+
+
+while n < user_floor.to_i
+	puts ("#" * n).rjust(user_floor.to_i-1)
+	n += 1
+
+end
